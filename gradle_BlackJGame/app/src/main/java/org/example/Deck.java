95baa3428a4db6b68dee2d015286b0c4e3da04e1
HyoungJunCH <chj073675@gmail.com>
2024-11-16 15:00:18 +0900

    클래스 수정 	modified:   Proposal.md 	modified:   gradle_BlackJGame/app/src/main/java/org/example/Deck.java

--- gradle_BlackJGame/app/src/main/java/org/example/Deck.java
+++ gradle_BlackJGame/app/src/main/java/org/example/Deck.java
@@ -1,10 +1,9 @@
 package org.example;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Stack;
-import java.util.ArrayList;;
 
 public class Deck {
-    //private Card[] cards; //덱의 카드 배열
     private Stack<Card> cards;
     private int stacks; //덱의 카드 수
     private int sets; //덱의 세트 수
@@ -17,17 +16,14 @@ public class Deck {
             this.sets = 2; //덱의 세트 수 default값 설정
             this.stacks = 52 * this.sets;
         }
-        //this.cards = new Card[stacks]; //카드 배열 생성
         this.cards = new Stack<>();
 
         String[] suits = {"♠", "♣", "♥", "♦"}; //카드 모양 배열
         String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}; //카드 숫자 배열
 
-        //int index = 0; //카드 배열 인덱스
         for (int i = 0; i < sets; i++) { 
             for (String suit : suits) { 
                 for (String rank : ranks) { 
-                    //cards[index++] = new Card(suit, rank); //카드 생성
                     cards.push(new Card(suit, rank));
                 }
             }
